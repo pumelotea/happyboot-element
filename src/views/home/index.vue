4db9007702c3ba4b5b077e5de3afa1f64aa3b952
pumelotea <zf_722@yeah.net>
2021-01-26 09:59:24 +0800

    fix: keep-alive

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,15 +10,13 @@
       <MenuList />
     </template>
     <template v-slot:content>
-      <content-container ref="cache">
-        <router-view   v-slot="{ Component }" >
-<!--          <keep-alive>-->
-            <component :is="Component" :pageId="pageId" :key="pageId"></component>
-<!--          </keep-alive>-->
+      <content-container >
+        <router-view  v-slot="{ Component }" >
+          <keep-alive ref="aaa">
+            <component v-if="isKeepalive" :is="Component" :pageId="pageId" :isKeepalive="isKeepalive" :key="pageId"></component>
+          </keep-alive>
+          <component v-if="!isKeepalive" :is="Component" :pageId="pageId" :isKeepalive="isKeepalive" :key="pageId"></component>
         </router-view>
-<!--        <router-view v-slot="{ Component }" v-if="!isKeepalive">-->
-<!--          <component  :is="Component" :pageId="pageId"  :key="pageId"></component>-->
-<!--        </router-view>-->
       </content-container>
     </template>
   </main-layout>
@@ -30,9 +28,9 @@ import NavBar from '@/components/NavBar.vue'
 import MainLayout from '@/layouts/MainLayout.vue'
 import MenuList from '@/components/MenuList.vue'
 import ContentContainer from '@/layouts/ContentContainer.vue'
-import { defineComponent, computed } from 'vue'
+import { defineComponent, watch, ref, onMounted, getCurrentInstance } from 'vue'
 import { getHappykitInstance } from '@/framework'
-
+import happyKitRouter from '@/router'
 export default defineComponent({
   components: {
     MainLayout,
@@ -43,19 +41,31 @@ export default defineComponent({
   },
   setup() {
     const hkf = getHappykitInstance()
-    const currentRouteMenu = hkf.getCurrentMenuRoute()
-    const isKeepalive = computed(() => {
-      return currentRouteMenu.value?.menuItem.isKeepalive || false
+    const isKeepalive = ref(false)
+    const pageId = ref('')
+    // const ctx = (getCurrentInstance() as any).ctx
+
+    const update = ()=>{
+      pageId.value = hkf.getCurrentMenuRoute().value?.pageId || ''
+      isKeepalive.value = hkf.getCurrentMenuRoute().value?.menuItem.isKeepalive || false
+      // console.log((ctx.$refs.aaa as any))
+    }
+
+    watch(happyKitRouter.currentRoute,()=>{
+      update()
     })
 
-    const pageId = computed(() => {
-      return currentRouteMenu.value?.pageId || ''
+    onMounted(()=>{
+      update()
     })
 
     return {
       pageId,
-      isKeepalive
+      isKeepalive,
     }
+  },
+  mounted() {
+    // console.log((this.$refs.aaa as any))
   }
 })
 </script>
