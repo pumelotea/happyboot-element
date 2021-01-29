27b72cd2441bf59747154591b6d7c73a322bbd93
lichunhui <380060615@qq.com>
2021-01-29 09:57:43 +0800

    add: 用户管理模块

--- src/components.ts
+++ src/components.ts
@@ -2,15 +2,19 @@ import {App} from 'vue'
 import PageLayout from '@/layouts/PageLayout.vue'
 import PageLayoutFull from '@/layouts/PageLayoutFull.vue'
 import PageIframeLayout from '@/layouts/PageIframeLayout.vue'
+import FormItemContainer from '@/layouts/FormItemContainer.vue'
+import DrawerLayout from '@/layouts/DrawerLayout.vue'
 import NoData from '@/components/NoData.vue'
 
 
 
 export default {
   install(app: App){
-    app.component('page-layout', PageLayout)
-    app.component('page-layout-full', PageLayoutFull)
-    app.component('page-iframe-layout', PageIframeLayout)
-    app.component('no-data', NoData)
+    app.component('page-layout', PageLayout as any)
+    app.component('page-layout-full', PageLayoutFull as any)
+    app.component('page-iframe-layout', PageIframeLayout as any)
+    app.component('no-data', NoData as any)
+    app.component('form-item-container', FormItemContainer as any)
+    app.component('drawer-layout', DrawerLayout as any)
   }
 }
