8599ace411a8cf942f644bde521474d9959988a3
pumelotea <zf_722@yeah.net>
2021-02-03 08:35:39 +0800

    chore: 清理代码

--- public/config/index.js
+++ public/config/index.js
@@ -1,7 +1,6 @@
 window.api = {
   dev: {
     server: 'http://10.10.0.27:6630/system'
-    // server: 'http://www.jpldx.com:6630/system'
   },
   prod: {
     server: 'https://www.jpldx.com:8443/system-api/'
