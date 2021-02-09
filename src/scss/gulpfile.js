6062a3934dea3c8ab026517941229d7119e4b7ba
pumelotea <zf_722@yeah.net>
2021-02-09 22:15:20 +0800

    feat(theme): 主题编译改进，主题优化

--- src/scss/gulpfile.js
+++ src/scss/gulpfile.js
@@ -5,28 +5,47 @@ const sass = require('gulp-dart-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
-
-const themes = ['dark','light']
+const through = require('through2');
+const fs = require('fs')
 
 const noElPrefixFile = /(index|base|display|default)/
 
-function compile() {
-  return src('./src/default.scss')
-    .pipe(sass.sync())
-    .pipe(autoprefixer({ cascade: false }))
-    .pipe(cssmin())
-    .pipe(rename(function (path) {
-      if(!noElPrefixFile.test(path.basename)) {
-        path.basename = `el-${path.basename}`
-      }
-    }))
-    .pipe(dest('../../public/theme/dark'))
+const themes = ['dark','light']
+
+function compileTheme(){
+  const compiler = []
+  themes.forEach(theme =>{
+    function compile() {
+      return src(['./src/common/var.scss','./src/default.scss'])
+        .pipe(through.obj(function(file, encode, cb) {
+          const filename = file.relative
+          if (filename === 'var.scss'){
+            fs.writeFileSync('./src/common/var.scss',`@import "var-${theme}";`)
+          }else {
+            this.push(file)
+          }
+          cb()
+        }))
+        .pipe(sass.sync())
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(cssmin())
+        .pipe(rename(function (path) {
+          if(!noElPrefixFile.test(path.basename)) {
+            path.basename = `el-${path.basename}`
+          }
+        }))
+        .pipe(dest(`../../public/theme/${theme}`))
+    }
+
+    function copyfont() {
+      return src('./src/fonts/**')
+        .pipe(cssmin())
+        .pipe(dest(`../../public/theme/${theme}/fonts`))
+    }
+    compiler.push(compile,copyfont)
+  })
+  return compiler
 }
 
-function copyfont() {
-  return src('./src/fonts/**')
-    .pipe(cssmin())
-    .pipe(dest('../../public/theme/dark/fonts'))
-}
 
-exports.build = series(compile, copyfont)
+exports.build = series(...compileTheme())
--- src/scss/index.ts
+++ src/scss/index.ts
@@ -1,9 +1,9 @@
 import store from '@/store'
 
 
-if (process.env.VUE_APP_BUILT_MODE === 'dev'){
-  require('@/scss/src/default.scss')
-}
-if (process.env.VUE_APP_BUILT_MODE === 'prod'){
+// if (process.env.VUE_APP_BUILT_MODE === 'dev'){
+//   require('@/scss/src/default.scss')
+// }
+// if (process.env.VUE_APP_BUILT_MODE === 'prod'){
   store.commit('initTheme')
-}
+// }
