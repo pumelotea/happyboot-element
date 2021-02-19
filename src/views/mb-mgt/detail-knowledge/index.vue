7dd28fa5a8c20819e30558c78374f4785489ef9a
chunhuili12138 <32325859+chunhuili12138@users.noreply.github.com>
2021-02-19 08:09:09 +0800

    feat: knowledge module (#19)

---
+++ src/views/mb-mgt/detail-knowledge/index.vue
@@ -0,0 +1,320 @@
+<template>
+  <page-layout-full v-loading="loading" style="padding: 0">
+    <div class="message" v-if="detail">
+      <div
+        class="poster"
+        :style="'background: url('+imgId2Url(detail.imageArr[0])+') no-repeat;'" >
+        <div class="post-head-content">
+          <div style="margin: auto">
+            <h1 class="title">{{ detail.title }}</h1>
+            <div class="meta">
+              <div class="type">{{ labelMap[detail.label] }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="content post-content"
+        v-highlight
+        v-html="detail.content"
+      ></div>
+    </div>
+    <div class="no-data" v-if="!loading && !detail"></div>
+  </page-layout-full>
+</template>
+
+<script lang='ts'>
+import { computed, defineComponent, onMounted, ref } from 'vue'
+import { self } from '@/common'
+
+export default defineComponent ({
+  name: 'index',
+  setup() {
+    const context = self()
+
+    const loading = ref(false)
+    const detail: any = ref(null)
+    const labelMap: any = ref({})
+
+    let knowledgeId = computed(() => {
+      return context.$route.query.knowledgeId || ''
+    })
+
+    const getDict = async () => {
+      const res: any = await context.$api.dictItemsMap('KNOWLEDGE_LABEL')
+      if (res.code === 0) {
+        res.data.KNOWLEDGE_LABEL.forEach((e: any) => {
+          labelMap.value[e.value] = e.label
+        })
+      }
+    }
+
+    const getKnowledgeDetail = async () => {
+      const res: any = await context.$api.detailKnowledge(knowledgeId)
+      if (res.code === 0) {
+        detail.value = res.data
+      }
+      loading.value = false
+    }
+
+    const imgId2Url = (url: any) => {
+      return context.$imgId2Url(url)
+    }
+
+    onMounted(async () => {
+      await getDict()
+      await getKnowledgeDetail()
+    })
+
+    return {
+      imgId2Url,
+      knowledgeId,
+      loading,
+      detail,
+      labelMap
+    }
+  }
+})
+</script>
+
+<style scoped>
+.message {
+  overflow: auto;
+}
+.title {
+  text-align: center;
+  color: white;
+}
+
+.meta {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+
+.type,
+.author,
+.sent-time {
+  padding: 0 10px;
+  font-size: 14px;
+  color: white;
+}
+
+.content {
+  padding: 30px;
+  max-width: 768px;
+  margin: auto;
+}
+
+.poster {
+  height: 400px;
+  display: flex;
+  background-size: cover;
+  background-position:center;
+}
+.post-head-content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background: rgba(0, 0, 0, 0.4);
+}
+</style>
+<style>
+/*文章样式*/
+.post-content {
+  word-break: break-all;
+  padding: 1.2rem;
+  color: slategrey;
+}
+.post-content p {
+  font-weight: 400 !important;
+  color: #526f86 !important;
+  margin-bottom: 16px;
+  line-height: 20px !important;
+}
+
+.post-content h1 {
+  color: #4b5157 !important;
+  margin-bottom: 16px;
+}
+
+.post-content h2 {
+  border-bottom: 1px solid #c9c9c9;
+  margin-bottom: 16px;
+  color: #4b5157 !important;
+}
+
+.post-content h3 {
+  color: #4b5157 !important;
+  margin-bottom: 16px;
+  position: relative;
+  padding-left: 15px;
+}
+
+.post-content h3::before {
+  content: '';
+  position: absolute;
+  top: 10px;
+  left: 0;
+  height: 8px;
+  width: 8px;
+  border-radius: 20px;
+  background-color: #1951fc;
+}
+
+.post-content h4 {
+  color: #4b5157 !important;
+  margin-bottom: 16px;
+}
+
+.post-content h5 {
+  color: #4b5157 !important;
+  margin-bottom: 16px;
+}
+
+.post-content ol {
+  color: #4b5157 !important;
+  margin-bottom: 16px;
+}
+
+.post-content ol li {
+  margin-bottom: 10px;
+}
+
+.post-content blockquote {
+  padding: 0.5rem 1.8rem;
+  font-size: 1.1rem;
+  border-left: 5px solid #1951fc;
+  color: #495057;
+  background: #edf0f3;
+  font-weight: 400;
+  margin-bottom: 16px;
+}
+
+.post-content blockquote p {
+  margin-top: 1rem;
+  margin-bottom: 0rem;
+}
+
+.post-content ul {
+  color: #495057;
+  margin-bottom: 16px;
+}
+
+.post-content ul li {
+  margin-bottom: 10px;
+}
+
+.post-content ul li p {
+  margin-bottom: 0;
+}
+
+.post-content ol li p {
+  margin-bottom: 0;
+}
+
+.post-content ol li code {
+  margin-bottom: 0;
+}
+
+.post-content ul li code {
+  margin-bottom: 0;
+}
+
+.post-content ol li pre {
+  margin-bottom: 0;
+}
+
+.post-content ul li pre {
+  margin-bottom: 0;
+}
+
+.post-content strong {
+  font-weight: bolder;
+}
+
+.post-content p code {
+  margin-bottom: 0;
+  padding: 0;
+  box-shadow: none;
+  background-color: #e9ecef;
+  overflow: initial !important;
+  font-family: Monaco, Monospaced, sans-serif;
+  font-size: 13px;
+}
+
+.post-content code {
+  margin-bottom: 16px;
+  padding: 0;
+  /*line-height: 30px;*/
+  box-shadow: none !important;
+  background-color: #e9ecef !important;
+  font-family: Monaco, Monospaced, sans-serif;
+  font-size: 13px !important;
+}
+
+.post-content pre {
+  /*background: #292929;*/
+  padding: 1rem;
+  /*color: #f1f1f1;*/
+  border-radius: 3px;
+  margin-bottom: 16px;
+  background-color: #e9ecef;
+  font-family: Monaco, Monospaced, sans-serif;
+  font-size: 13px;
+}
+
+.post-content img {
+  max-width: 100%;
+  height: auto;
+  margin-bottom: 16px;
+}
+
+.post-content a {
+  text-decoration-line: none;
+}
+
+.post-content a:hover {
+  color: #136cfc;
+}
+
+.post-content hr {
+  margin-bottom: 16px;
+}
+
+.post-content table {
+  border-collapse: collapse;
+  margin: 0 0 1rem;
+  word-break: normal;
+}
+
+.post-content th,
+.post-content td {
+  border: 1px solid #eee;
+  padding: 8px 12px;
+}
+
+.post-content iframe,
+embed {
+  border: 0;
+  display: block;
+  overflow: hidden;
+}
+
+.post-content figure {
+  margin: 0;
+}
+
+.post-content video {
+  width: 100%;
+  margin: auto;
+}
+
+.no-data {
+  height: 100%;
+  background: url('../../../../public/images/404.svg') no-repeat;
+  background-position: center;
+  background-size: 40% 40%;
+}
+</style>
---
+++ src/views/mb-mgt/knowledge/index.vue
@@ -0,0 +1,357 @@
+<template>
+  <page-layout>
+    <template #actions>
+      <el-row :gutter="10">
+        <el-col :md="6">
+          <form-item-container :label="'标题'">
+            <el-input
+              placeholder="请输入标题"
+              v-model="tableData.searchQuery.title"
+            />
+          </form-item-container>
+        </el-col>
+        <el-col :md="6">
+          <form-item-container :label="'标签'">
+            <el-select
+              v-model="tableData.searchQuery.label"
+              clearable
+              placeholder="请选择"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="(item, key) in esLabelOptions"
+                :key="key"
+                :label="item"
+                :value="key"
+              />
+            </el-select>
+          </form-item-container>
+        </el-col>
+      </el-row>
+      <el-row :gutter="10" style="margin-top: 15px">
+        <el-col>
+          <el-button
+            type="primary"
+            permission-key="add"
+            size="medium"
+            @click="handleAdd"
+            >新增</el-button
+          >
+          <el-button type="primary" size="medium" @click="handleSearch"
+            >查询</el-button
+          >
+          <el-button type="primary" size="medium" plain @click="handleReset"
+            >重置</el-button
+          >
+          <el-button
+            permission-key="delete"
+            type="danger"
+            size="medium"
+            @click="handleMultiDelete"
+            v-show="tableSelectedData.length > 0"
+            >批量删除</el-button
+          >
+        </el-col>
+      </el-row>
+    </template>
+    <el-table
+      size="mini"
+      :data="tableData.list"
+      @selection-change="tableSelected"
+      border
+      v-loading="tableData.loading"
+      style="width: 100%"
+    >
+      <el-table-column type="selection" align="center" width="50" />
+      <el-table-column type="index" align="center" width="50" label="#" />
+      <el-table-column prop="title" align="center" label="标题" />
+      <el-table-column prop="label" align="center" label="标签">
+        <template #default="scope">{{
+          esLabelOptions[scope.row.label]
+        }}</template>
+      </el-table-column>
+      <el-table-column prop="content" align="center" label="内容">
+        <template #default="scope">{{ formatText(scope.row.content) }}</template>
+      </el-table-column>
+      <el-table-column fixed="right" align="center" label="操作" width="150">
+        <template #default="scope">
+          <el-button @click="handleDetail(scope.row)" type="text"
+            >查看</el-button
+          >
+          <el-button
+            @click="handleEdit(scope.row)"
+            type="text"
+            permission-key="edit"
+            >编辑</el-button
+          >
+          <el-button
+            @click="handleDelete(scope.row)"
+            type="text"
+            permission-key="delete"
+            style="color: red"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <template #pagination>
+      <el-pagination
+        :page-sizes="[20, 50, 100]"
+        :page-size="tableData.searchQuery.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="tableData.total"
+        @size-change="pageSizeChange"
+        @current-change="currentClick"
+      />
+    </template>
+    <form-drawer ref="FD" @handleSubmit="handleSearch" />
+    <detail-drawer ref="DD" />
+  </page-layout>
+</template>
+<script lang='ts'>
+import { defineComponent, ref, reactive, onMounted } from 'vue'
+import { self } from '@/common'
+import FormDrawer from './drawer/FormDrawer.vue'
+import DetailDrawer from './drawer/DetailDrawer.vue'
+import { timeFormat } from '@/common/utils'
+export default defineComponent ({
+  name: 'index',
+  components: {
+    FormDrawer,
+    DetailDrawer
+  },
+  setup() {
+    const context = self()
+
+    const FD: any = ref(null)
+    const DD: any = ref(null)
+
+    const tableData = reactive({
+      searchQuery: {
+        label: '',
+        title: '',
+        pageNo: 1,
+        pageSize: 20
+      },
+      total: 0,
+      loading: false,
+      list: []
+    })
+    const esLabelOptions: any = ref({})
+    const tableSelectedData: any = ref([])
+
+    //初始化字典
+    const getDict = async (success: any, error: any) => {
+      const res: any = await context.$api.dictItemsMap('KNOWLEDGE_LABEL')
+      if (res.code === 0) {
+        success(res.data)
+      } else {
+        error()
+      }
+    }
+
+    //查询
+    const search = async (success: any, error: any) => {
+      tableData.loading = true
+      const res: any = await context.$api.getKnowledge(tableData.searchQuery)
+      if (res.code === 0) {
+        success(res)
+      } else {
+        error(res)
+      }
+      tableData.loading = false
+    }
+
+    // 删除 可批量删除，id用,隔开
+    const doDelete = async (id: any, success: any, error: any) => {
+      tableData.loading = true
+      const res: any = await context.$api.deleteKnowledge(id)
+      if (res.code === 0) {
+        success(res)
+      } else {
+        error(res)
+      }
+      tableData.loading = false
+    }
+
+    const initDict = () => {
+      //初始化字典
+      getDict(
+          (success: any) => {
+            let temp: any = {}
+            for (let i = 0; i < success.KNOWLEDGE_LABEL.length; i++) {
+              temp[success.KNOWLEDGE_LABEL[i].value] =
+                  success.KNOWLEDGE_LABEL[i].label
+            }
+            esLabelOptions.value = temp
+          },
+          (err: any) => {
+            context.$notify({
+              type: 'error',
+              title: '提示',
+              message: err.msg
+            })
+          }
+      )
+    }
+
+    const pageSizeChange = (val: any) => {
+      tableData.searchQuery.pageSize = val
+      tableData.searchQuery.pageNo = 1
+      handleSearch()
+    }
+
+    const currentClick = (val: any) => {
+      tableData.searchQuery.pageNo = val
+      handleSearch()
+    }
+
+    const handleSearch = () => {
+      search(
+          (res: any) => {
+            tableData.list = res.data.records
+            tableData.total = Number(res.data.total)
+          },
+          (err: any) => {
+            context.$notify({
+              type: 'error',
+              title: '提示',
+              message: err.msg
+            })
+          }
+      )
+    }
+
+    const tableSelected = (rows: any) => {
+      tableSelectedData.value = rows
+    }
+
+    const handleAdd = () => {
+      (FD.value as any).add()
+    }
+
+    const handleEdit = (row: any) => {
+      (FD.value as any).edit({ id: row.id })
+    }
+
+    const handleDetail = (row: any) => {
+      context.$router.push({path:'/mb-mgt/knowledge-detail',query:{knowledgeId:row.id}}, row.esTitle)
+    }
+
+    // 单删
+    const handleDelete = (row: any) => {
+      const msg = '即将删除该条数据, 是否确认?'
+      const ids = row.id
+      implementDelete(ids, msg)
+    }
+
+    // 多删
+    const handleMultiDelete = () => {
+      const msg =
+          '即将删除这' + tableSelectedData.value.length + '条数据, 是否确认?'
+      let ids = ''
+      tableSelectedData.value.forEach((item: any) => {
+        ids = ids + ',' + item.id
+      })
+      implementDelete(ids, msg)
+    }
+
+    // 执行删除
+    const implementDelete = (ids: any, msg: any) => {
+      context.$confirm(msg, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        doDelete(
+          ids,
+          () => {
+            tableData.searchQuery.pageNo = 1
+            handleSearch()
+            context.$notify({
+              type: 'success',
+              title: '提示',
+              message: '删除成功！'
+            })
+          },
+          (err: any) => {
+            context.$notify({
+              type: 'error',
+              title: '提示',
+              message: err.msg
+            })
+          }
+        )
+      })
+    }
+
+    //重置
+    const handleReset = () => {
+      tableData.searchQuery.label = ''
+      tableData.searchQuery.title = ''
+      tableData.searchQuery.pageNo = 1
+      tableData.searchQuery.pageSize = 20
+
+      handleSearch()
+    }
+
+    const formatTime = (val: any) => {
+      if (!val) {
+        return ''
+      }
+      return timeFormat(val)
+    }
+
+    const formatText = (val: any) => {
+      if (!val) {
+        return ''
+      }
+      if (val.length > 20) {
+        return val.substring(0, 20) + '...'
+      }
+      return val
+    }
+
+    onMounted(() => {
+      initDict()
+      handleSearch()
+    })
+
+    return {
+      getDict,
+      initDict,
+      search,
+      doDelete,
+      pageSizeChange,
+      currentClick,
+      handleSearch,
+      tableSelected,
+      handleAdd,
+      handleEdit,
+      handleDetail,
+      handleDelete,
+      handleMultiDelete,
+      handleReset,
+      implementDelete,
+      formatTime,
+      formatText,
+      tableData,
+      esLabelOptions,
+      tableSelectedData,
+      FD,
+      DD
+    }
+  }
+})
+</script>
+
+<style scoped>
+.el-dropdown-link {
+  cursor: pointer;
+  margin-left: 10px;
+}
+
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+</style>
