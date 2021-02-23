70123368f36db4439d723775699a30b863bf87bf
pumelotea <zf_722@yeah.net>
2021-02-23 12:51:32 +0800

    fix: page重置方法支持数据加载

--- src/common/page.ts
+++ src/common/page.ts
@@ -105,6 +105,7 @@ export function createPage(options: PageOption) {
         pageData.searchCondition[dataKey] = conditions![dataKey].reset
       })
     }
+    dataLoader && dataLoader(pageData.searchCondition)
   }
 
   const pageConditionSearch = () => {
