b01409ffba106ce9e5dde684005fe47474e7dbfb
pumelotea <zf_722@yeah.net>
2021-03-04 10:26:53 +0800

    style: 格式化代码

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,26 +1,26 @@
 <template>
   <hb-page-layout>
     <el-row :gutter="20">
-      <el-col :span="24"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
+      <el-col :span="24"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="6"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="6"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="12"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
+      <el-col :span="6"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="6"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="12"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="3"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="3"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
     </el-row>
     <el-row :gutter="20">
-      <el-col :span="12"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
-      <el-col :span="12"><el-card shadow="hover" :body-style='bodyStyle'><div class='div-content'></div></el-card></el-col>
+      <el-col :span="12"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
+      <el-col :span="12"><el-card shadow="hover" :body-style="bodyStyle"><div class="div-content"></div></el-card></el-col>
     </el-row>
   </hb-page-layout>
 </template>
